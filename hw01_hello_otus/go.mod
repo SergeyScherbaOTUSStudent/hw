8cb759871ea36126834052781912f77c16e91240
Sergey SS <w.sergey89@gmail.com>
2023-04-03 10:52:04 +0300

    review

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,4 +1,4 @@
-module github.com/SergeyScherbaOTUSStudent/hw/tree/master/hw01_hello_otus
+module github.com/SergeyScherbaOTUSStudent/hw/hw01_hello_otus
 
 go 1.19
 
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -7,6 +7,5 @@ import (
 )
 
 func main() {
-	strReverse := stringutil.Reverse("Hello, OTUS!")
-	fmt.Println(strReverse)
+	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
 }
