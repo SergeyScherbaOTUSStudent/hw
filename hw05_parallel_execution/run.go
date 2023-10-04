7164e0a551790342d872ab00b1f87a185a3d2ee0
Sergey SS <w.sergey89@gmail.com>
2023-10-04 11:35:50 +0300

    hw05_parallel_execution #solution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,15 +1,60 @@
-package hw05parallelexecution
+package main
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+var InvalidMParam = errors.New("invalid params. Give chance to mistake for app :)")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+func Run(tasks []Task, n, m int) (e error) {
+	if m <= 0 {
+		return InvalidMParam
+	}
+
+	var count int32
+
+	taskCh := make(chan Task)
+	exit := make(chan bool)
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case <-exit:
+					return
+				default:
+					t, ok := <-taskCh
+					if !ok {
+						return
+					}
+					if err := t(); err != nil {
+						atomic.AddInt32(&count, 1)
+					}
+				}
+			}
+		}()
+	}
+
+	wg.Add(n)
+	for _, task := range tasks {
+		if atomic.LoadInt32(&count) >= int32(m) {
+			exit <- true
+			e = ErrErrorsLimitExceeded
+			break
+		}
+
+		taskCh <- task
+		continue
+	}
+	close(taskCh)
+	wg.Wait()
+
+	return
 }
