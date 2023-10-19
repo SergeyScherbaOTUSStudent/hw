3d870100c34354637ae65b91c401d45dd8ec0cfc
Sergey SS <w.sergey89@gmail.com>
2023-10-19 16:10:57 +0300

    hw07_file_copying solution without iterate of byte

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,73 @@ package main
 
 import (
 	"errors"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrFileDoesNotExist      = errors.New("file does not exist")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	var err error
+	err = CheckFile(fromPath)
+
+	if err != nil {
+		return err
+	}
+
+	bar := pb.StartNew(100)
+
+	finalBoard := int(limit)
+	fContent, readErr := os.ReadFile(fromPath)
+
+	if readErr != nil {
+		return ErrUnsupportedFile
+	}
+
+	if len(fContent) < int(offset) {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if offset > 0 {
+		finalBoard = int(offset + limit)
+	}
+
+	if len(fContent) < int(offset+limit) || limit == 0 {
+		finalBoard = len(fContent)
+	}
+
+	printPB(bar, 50)
+
+	toFile, _ := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE, 0o666)
+
+	defer func() {
+		printPB(bar, 100)
+		toFile.Close()
+	}()
+
+	_, err = toFile.Write(fContent[offset:finalBoard])
+	printPB(bar, 96)
+
+	return err
+}
+
+func CheckFile(path string) error {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return ErrFileDoesNotExist
+		}
+	}
+
 	return nil
+}
+
+func printPB(bar *pb.ProgressBar, n int) {
+	bar.Add(n)
+	time.Sleep(time.Millisecond * 10)
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,52 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	from = path.Join("testdata", "input.txt")
+
+	testcases := []struct {
+		title     string
+		from      string
+		reference string
+		offset    int64
+		limit     int64
+	}{
+		{"Test_Offset0_limit0", from, "testdata/out_offset0_limit0.txt", 0, 0},
+		{"Test_Offset0_limit10", from, "testdata/out_offset0_limit10.txt", 0, 10},
+		{"Test_Offset0_limit1000", from, "testdata/out_offset0_limit1000.txt", 0, 1000},
+		{"Test_Offset0_limit10000", from, "testdata/out_offset0_limit10000.txt", 0, 10000},
+		{"Test_Offset100_limit1000", from, "testdata/out_offset100_limit1000.txt", 100, 1000},
+		{"Test_Offset6000_limit1000", from, "testdata/out_offset6000_limit1000.txt", 6000, 1000},
+	}
+
+	for _, testcase := range testcases {
+		to := path.Join("testdata", "out_tmp.txt")
+		t.Run(testcase.title, func(t *testing.T) {
+			err := Copy(testcase.from, to, testcase.offset, testcase.limit)
+			defer os.Remove(to)
+
+			a, _ := os.ReadFile(to)
+			b, _ := os.ReadFile(testcase.reference)
+
+			require.NoError(t, err)
+			require.Equal(t, a, b)
+		})
+	}
+
+	t.Run("file does not exist", func(t *testing.T) {
+		err := Copy("testdata/input_.txt", "to", 0, 0)
+		require.ErrorIs(t, err, ErrFileDoesNotExist)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "to", 10000000, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
 }
